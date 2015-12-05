0444a638c418674feff79472eab28c2ccf96a890
Tarasinf <tarasinf2013@gmail.com>
2015-12-05 12:34:08 +0200

    Fixed redis run.

--- eq/settings.py
+++ eq/settings.py
@@ -130,13 +130,6 @@ STATICFILES_DIRS = (
 BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 
-
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': (
-#         'rest_framework.authentication.BasicAuthentication',
-#     ),
-# }
-
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
