2920b038770ca1e08d189a5f54cd4dd7fa852c0f
Tarasinf <tarasinf2013@gmail.com>
2016-02-17 23:28:29 +0200

    Added functional for creating Week and Month report from EmotionStates

--- app_eq_1/tasks.py
+++ app_eq_1/tasks.py
@@ -5,18 +5,21 @@ from celery import task
 from app_eq_1.models import UserCourse
 from app_eq_1.models import Lesson
 from app_eq_1.models import Action
+from app_eq_1.models import Conclusions
+from app_eq_1.models import WeeklyReport, MonthlyReport
 from django.contrib.auth.models import User
-import datetime
+from datetime import timedelta, datetime
+from django.utils import timezone
+from app_eq_1.constants import USER_EMOTIONS, USER_ACTIVITY
 
 
 @task()
 def example():
-    print '[ EVERY_30_SECONDS ] [ %s ]' % (str(datetime.datetime.now().time()),)
-
+    print '[ EVERY_30_SECONDS ] [ %s ]' % (str(datetime.now().time()),)
 
 @task()
 def every_day():
-    print '[ EVERY_DAY ] [ %s ]' % (str(datetime.datetime.now().time()),)
+    print '[ EVERY_DAY ] [ %s ]' % (str(datetime.now().time()),)
 
     try:
         for item in UserCourse.objects.filter(deleted=False).filter(status='active').distinct():
@@ -28,7 +31,7 @@ def every_day():
 
                     item.last_lesson += 1
                     item.save()
-                    send_email(item.course.lesson_set.get(number=item.last_lesson).id, item.user.id)
+                    send_email_lesson(item.course.lesson_set.get(number=item.last_lesson).id, item.user.id)
                 else:
                     item.status = 'ended'
                     item.save()
@@ -40,38 +43,125 @@ def every_day():
 
 @task()
 def every_week():
-    print '[ EVERY_WEEK ] [ %s ]' % (str(datetime.datetime.now().time()),)
+    print '[ EVERY_WEEK ] [ %s ]' % (str(datetime.now().time()),)
     try:
+        some_day_last_week = datetime.now().date() - timedelta(days=7)
+        monday_of_last_week = some_day_last_week - timedelta(days=(some_day_last_week.isocalendar()[2] - 1))
+        monday_of_this_week = monday_of_last_week + timedelta(days=7)
 
-        EMAIL_SUBJECT = 'TEST EMAIL'
-        EMAIL_MESSAGE = u'Привіт усім, це перевірка роботи щотижневого звіту.'
-        EMAIL_EMAIL_FROM = 'eq@eq.com'
-        EMAIL_EMAIL_TO = 'tarasinf2013@gmail.com'
+        for user in User.objects.all():
+            try:
+                ratings = {}
+                for emotion in USER_EMOTIONS:
+                    temp = {}
+                    for activity in USER_ACTIVITY:
+                        temp[activity[0]] = 0
+                    ratings[emotion[0]] = temp
 
-        from django.core.mail import send_mail
-        send_mail(EMAIL_SUBJECT, EMAIL_MESSAGE, EMAIL_EMAIL_FROM, [EMAIL_EMAIL_TO], fail_silently=False)
+                emotional_states = user.emotionalstate_set.filter(
+                    updated__gte=monday_of_last_week,
+                    updated__lt=monday_of_this_week)
+                if not emotional_states:
+                    continue
+                for emotional_state in emotional_states:
+                    ratings[emotional_state.emotion][emotional_state.activity] += emotional_state.subjectivity * emotional_state.confidence
+
+                winner = 0
+                winner_activity = None
+                winner_emotion = None
+                for emotion in ratings:
+                    for activity in ratings[emotion]:
+                        if ratings[emotion][activity] > winner:
+                            winner = ratings[emotion][activity]
+                            winner_activity = activity
+                            winner_emotion = emotion
+
+                if winner:
+                    conclusion = Conclusions.objects.\
+                        filter(activity__exact=winner_activity).\
+                        filter(emotion__exact=winner_emotion)[0]
+                    # create report
+                    template_week_report = 'This is week report: %s' % conclusion.text
+                    report = WeeklyReport.objects.create(
+                        user=user,
+                        text=template_week_report
+                    )
+                    # send email
+                    send_email_report_week(
+                        week_report_id=report.id,
+                        user_id=user.id
+                    )
+            except Exception, e:
+                print e
     except Exception, e:
-        print '[send_email]', e
+        print e
+
 
 @task
 def every_month():
-    print '[ EVERY_MONTH ] [ %s ]' % (str(datetime.datetime.now().time()),)
+    print '[ EVERY_MONTH ] [ %s ]' % (str(datetime.now().time()),)
     try:
+        some_day_last_month = datetime.now().date() - timedelta(days=28)
+        monday_of_last_month = some_day_last_month - timedelta(days=(some_day_last_month.isocalendar()[2] - 1))
+        monday_of_this_month = monday_of_last_month + timedelta(days=28)
+        print 'MMM', monday_of_last_month, monday_of_this_month
+        print 'P_0'
+        for user in User.objects.all():
+            try:
+                print 'P_1'
+                ratings = {}
+                for emotion in USER_EMOTIONS:
+                    temp = {}
+                    for activity in USER_ACTIVITY:
+                        temp[activity[0]] = 0
+                    ratings[emotion[0]] = temp
+                print 'P_2'
+                emotional_states = user.emotionalstate_set.filter(
+                    updated__gte=monday_of_last_month,
+                    updated__lt=monday_of_this_month)
+                if not emotional_states:
+                    continue
+                for emotional_state in emotional_states:
+                    ratings[emotional_state.emotion][emotional_state.activity] += emotional_state.subjectivity * emotional_state.confidence
+                print 'P_3'
+                winner = 0
+                winner_activity = None
+                winner_emotion = None
+                for emotion in ratings:
+                    for activity in ratings[emotion]:
+                        if ratings[emotion][activity] > winner:
+                            winner = ratings[emotion][activity]
+                            winner_activity = activity
+                            winner_emotion = emotion
 
-        EMAIL_SUBJECT = 'TEST EMAIL'
-        EMAIL_MESSAGE = u'Привіт усім, це перевірка роботи щомісячного звіту.'
-        EMAIL_EMAIL_FROM = 'eq@eq.com'
-        EMAIL_EMAIL_TO = 'tarasinf2013@gmail.com'
-
-        from django.core.mail import send_mail
-        send_mail(EMAIL_SUBJECT, EMAIL_MESSAGE, EMAIL_EMAIL_FROM, [EMAIL_EMAIL_TO], fail_silently=False)
+                if winner:
+                    print 'P_4'
+                    print 'Winner:', winner_activity, winner_emotion, winner
+                    conclusion = Conclusions.objects.\
+                        filter(activity__exact=winner_activity).\
+                        filter(emotion__exact=winner_emotion)[0]
+                    # create report
+                    template_month_report = 'This is month report: %s' % conclusion.text
+                    report = MonthlyReport.objects.create(
+                        user=user,
+                        text=template_month_report
+                    )
+                    print 'P_5'
+                    # send email
+                    send_email_report_month(
+                        month_report_id=report.id,
+                        user_id=user.id
+                    )
+                    print 'P_6'
+            except Exception, e:
+                print e
     except Exception, e:
-        print '[send_email]', e
+        print e
 
 
 @task()
-def send_email(lesson_id, user_id):
-    print '[ SEND EMAIL ] [ %s ]' % (str(datetime.datetime.now().time()))
+def send_email_lesson(lesson_id, user_id):
+    print '[ SEND EMAIL ] [ %s ]' % (str(datetime.now().time()))
 
     try:
         lesson = Lesson.objects.get(id=lesson_id)
@@ -85,4 +175,40 @@ def send_email(lesson_id, user_id):
         from django.core.mail import send_mail
         send_mail(EMAIL_SUBJECT, EMAIL_MESSAGE, EMAIL_EMAIL_FROM, [EMAIL_EMAIL_TO], fail_silently=False)
     except Exception, e:
-        print '[send_email]', e
+        print '[send_email_lesson]', e
+
+
+@task()
+def send_email_report_week(week_report_id, user_id):
+    print '[ SEND EMAIL WEEKLY REPORT ] [ %s ]' % (str(datetime.now().time()))
+
+    try:
+        report = WeeklyReport.objects.get(id=week_report_id)
+        user = User.objects.get(id=user_id)
+        EMAIL_SUBJECT = 'Week report [%s - %s]' % (str(report.updated), str(report.updated - timedelta(days=7)))
+        EMAIL_MESSAGE = report.text
+        EMAIL_EMAIL_FROM = 'eq@eq.com'
+        EMAIL_EMAIL_TO = user.email
+
+        from django.core.mail import send_mail
+        send_mail(EMAIL_SUBJECT, EMAIL_MESSAGE, EMAIL_EMAIL_FROM, [EMAIL_EMAIL_TO], fail_silently=False)
+    except Exception, e:
+        print '[send_email_weekly_report]', e
+
+
+@task()
+def send_email_report_month(month_report_id, user_id):
+    print '[ SEND EMAIL WEEKLY REPORT ] [ %s ]' % (str(datetime.now().time()))
+
+    try:
+        report = MonthlyReport.objects.get(id=month_report_id)
+        user = User.objects.get(id=user_id)
+        EMAIL_SUBJECT = 'Month report [%s - %s]' % (str(report.updated), str(report.updated - timedelta(days=28)))
+        EMAIL_MESSAGE = report.text
+        EMAIL_EMAIL_FROM = 'eq@eq.com'
+        EMAIL_EMAIL_TO = user.email
+
+        from django.core.mail import send_mail
+        send_mail(EMAIL_SUBJECT, EMAIL_MESSAGE, EMAIL_EMAIL_FROM, [EMAIL_EMAIL_TO], fail_silently=False)
+    except Exception, e:
+        print '[send_email_weekly_report]', e
