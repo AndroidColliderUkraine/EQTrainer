0813694d504006f77d85ff06d6a1210a83e37766
Tarasinf <tarasinf2013@gmail.com>
2016-07-03 12:18:21 +0300

    .

--- app_eq_1/static/js/my_profile.js
+++ app_eq_1/static/js/my_profile.js
@@ -1,14 +1,12 @@
 /*Menu-toggle*/
 $(document).ready(function(){
-	console.log('Test my sss');
 	var page = $("#temp_page").val();
 
 	$( ".temp_page_class").remove();
 	if (page){
 		$("#" + page).after('<i class="temp_page_class fa fa-check pull-right" aria-hidden="true"></i>');
 	}
-
-	console.log('Test my sss' + page);
+	
     $("#menu-toggle").click(function(e) {
         e.preventDefault();
         $("#wrapper").toggleClass("active");
