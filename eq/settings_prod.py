8a3a30d6789cb46edbff1e7470bdeaf16c51b58f
Taras <tarasinf2013@gmail.com>
2018-02-14 14:13:49 +0200

    .

--- eq/settings_prod.py
+++ eq/settings_prod.py
@@ -7,7 +7,7 @@ GOOGLE_ANALYTICS_ID = os.getenv('GOOGLE_ANALYTICS_ID', '')
 
 
 RAVEN_CONFIG = {
-    'dsn': os.getenv('SENTRY_DNS', ''),
+    'dsn': os.getenv('EQ_SENTRY_DNS', ''),
     # If you are using git, you can also automatically configure the
     # release based on the git info.
     # 'release': raven.fetch_git_sha(os.path.abspath(os.pardir)),
